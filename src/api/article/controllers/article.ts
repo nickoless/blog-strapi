15da1d6c548d90efa29e7bbcb6c21622d5f5f1f1
nickoless <nickoless.lee@gmail.com>
2024-10-24 15:08:53 -0400

    Create new strapi project, modify article api to search via slug instead of documentId

--- src/api/article/controllers/article.ts
+++ src/api/article/controllers/article.ts
@@ -4,4 +4,16 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('api::article.article');
+export default factories.createCoreController('api::article.article', ({ strapi }) => ({
+	async findOne(ctx) {
+		const { slug } = ctx.params;
+		const entity = await strapi.db.query('api::article.article').findOne({
+			where: { slug },
+			populate: ['blocks', 'categories', 'cover'],
+		});
+
+		const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+		return this.transformResponse(sanitizedEntity);
+	},
+}));
